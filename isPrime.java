bd3d19cd67f003230b69074819884535674e4830
rithanya-rit <139194192+rithanya-rit@users.noreply.github.com>
2023-07-12 20:47:09 +0530

    Add files via upload

---
+++ isPrime.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+// Read only region start
+class UserMainCode
+{
+    public int Solution(int input1) {
+        // Read only region end
+        // Write code here...
+        boolean isPrime - true;
+        for(int i = 2; i <= input1; i++) { // input1/2 & sqrt(input1) is a more optimised approach
+            if(input1 % i == 0) {
+                isPrime = false;
+                // use a break statement here
+            }
+        }
+
+        if (isPrime) {
+            return 2;
+        }
+
+        return 1;
+
+        // throw new UnsupportedOperationException("IsEven(int input1)");
+    }
+}
